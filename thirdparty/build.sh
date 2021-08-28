c58f68384073ece8edb82988ae5e8e535b85b930
q191201771 <191201771@qq.com>
2021-08-28 18:19:56 +0800

    FFmpeg rtmp h265 修改其中的flvenc.c

--- thirdparty/build.sh
+++ thirdparty/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-## 注释 CentOS下安装ffmpeg
+## 注释 MacOS或CentOS下安装ffmpeg
 ##
 
 set -x
+
+PREFIX=/usr/local
 
 ## 注释 cmake
 ##
@@ -14,53 +16,55 @@ set -x
 ##      https://www.videolan.org/developers/x264.html
 ##      https://code.videolan.org/videolan/x264/-/archive/master/x264-master.tar.bz2
 ##
-echo 'libx264...'
-tar jxvf x264-master.tar.bz2
-cd x264-master
-./configure --enable-shared --disable-asm
-make -j8 && make install && make clean
-cd -
+#echo 'libx264...'
+#tar jxvf x264-master.tar.bz2
+#cd x264-master
+#./configure --prefix=${PREFIX} --enable-shared --disable-asm --enable-pic --extra-cflags="-fno-stack-check"
+#make -j8 && make install && make clean
+#cd -
 
 ## 注释 libx265
 ##
 ##      https://github.com/videolan/x265/archive/refs/heads/master.zip
 ##
-echo 'x265...'
-unzip x265-master.zip
-cd x265-master/build
-cmake ../source
-make -j8 && make install && make clean
-cd -
+#echo 'x265...'
+#unzip x265-master.zip
+#cd x265-master/build
+#cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../source
+#make -j8 && make install && make clean
+#cd -
 
 ## 注释 fdk-aac
 ##
 ##      https://www.linuxfromscratch.org/blfs/view/svn/multimedia/fdk-aac.html
 ##      https://downloads.sourceforge.net/opencore-amr/fdk-aac-2.0.2.tar.gz
 ##
-echo 'fdk-aac...'
-tar zxvf fdk-aac-2.0.2.tar.gz
-cd fdk-aac-2.0.2
-./configure
-make -j8 && make install && make clean
-cd -
+#echo 'fdk-aac...'
+#tar zxvf fdk-aac-2.0.2.tar.gz
+#cd fdk-aac-2.0.2
+#./configure --prefix=${PREFIX}
+#make -j8 && make install && make clean
+#cd -
 
 ## 注释 opus
 ##
 ##      https://opus-codec.org/downloads/
 ##      https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
 ##
-echo 'opus...'
-tar zxvf opus-1.3.1.tar.gz
-cd opus-1.3.1
-./configure
-make -j8 && make install && make clean
-cd -
+#echo 'opus...'
+#tar zxvf opus-1.3.1.tar.gz
+#cd opus-1.3.1
+#./configure --prefix=${PREFIX}
+#make -j8 && make install && make clean
+#cd -
 
 ## 注释 ffmpeg
 ##
 ##      https://github.com/FFmpeg/FFmpeg/archive/refs/tags/n4.4.tar.gz
 ##
 ##      --extra-cflags="-fno-stack-check" https://pengrl.com/p/20042/
+##
+## 注释 注意，--enable-debug 这一行的选项打开用于编译debug版本
 ##
 echo 'ffmpeg...'
 tar zxvf n4.4.tar.gz
@@ -71,9 +75,14 @@ cp mod_ffmpeg_to_support_rtmp_hevc/libavformat/flvenc.c FFmpeg-n4.4/libavformat/
 cd FFmpeg-n4.4
 echo 'ffmpeg configure...'
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-./configure --enable-pic --extra-cflags="-fno-stack-check" --disable-x86asm \
-            --enable-gpl --enable-nonfree --enable-libx264 --enable-libx265 --enable-libfdk-aac --enable-libopus
-make -j8 && make install && make clean
+./configure --prefix=${PREFIX} --extra-cflags="-I${PREFIX}/include -L${PREFIX}/lib -fno-stack-check -g" \
+            --enable-pic \
+            --disable-x86asm \
+            --enable-shared --disable-static \
+            --enable-gpl --enable-nonfree \
+            --enable-libx264 --enable-libx265 --enable-libfdk-aac --enable-libopus \
+            --enable-debug=3 --disable-optimizations --disable-asm --disable-stripping
+make -j8 && make install
 cd -
 
 ## 注释 验证ffmpeg以及库是否能正常使用
